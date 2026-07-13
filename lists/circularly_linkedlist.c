7c1ab67f0e7790f2460a2ad6f235f73327c4ebc3
Sudharsan M S <sudharzz3049@gmail.com>
2026-07-13 22:36:52 +0530

    Circular Linked List v1

---
+++ lists/circularly_linkedlist.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+}*head;
+
+void display()
+{
+    struct Node *temp = head ->next;
+    if (temp == NULL)
+    {
+        printf("List is empty \n");
+        return;
+    }
+    do
+{
+    printf("%d ->", temp->data);
+    temp = temp->next;
+} while (temp != head->next);
+printf("(back to %d)\n",temp->data);
+
+printf("\n");
+}
+
+void insfirst(int val)
+{
+    struct Node *newnode = malloc(sizeof(struct Node));
+    newnode -> data = val;
+    if (head -> next == NULL)
+    {
+        head -> next = newnode;
+        newnode -> next = newnode;
+    }
+    else{
+        struct Node *temp = head -> next;
+        while(temp -> next != head -> next)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newnode;
+        newnode -> next = head -> next;
+        head->next = newnode;
+    }
+    printf("\n");
+}
+
+void inslast(int val)
+{
+    struct Node *newnode = malloc(sizeof(struct Node));
+    newnode -> data = val;
+    if (head -> next == NULL)
+    {
+        head -> next = newnode;
+        newnode -> next = newnode;
+    }
+    else{
+        struct Node *temp = head -> next;
+        while (temp -> next != head -> next){
+            temp = temp -> next;
+        }
+        temp -> next = newnode;
+        newnode -> next = head -> next;
+
+    }
+    printf("\n");
+}
+
+int main()
+{
+    head = malloc(sizeof(struct Node));
+    struct Node *first = malloc(sizeof(struct Node));
+    first -> data = 10;
+    head -> next = first;
+    struct Node *second = malloc(sizeof(struct Node));
+    second -> data = 20;
+    first -> next = second;
+    struct Node *third = malloc(sizeof(struct Node));
+    third -> data = 30;
+    second -> next = third;
+    third -> next = first;
+    insfirst(5);
+    display();
+    inslast(35);
+    display();
+}
