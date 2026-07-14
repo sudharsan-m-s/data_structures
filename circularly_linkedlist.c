dbef900d0d32591dd0d59309d4636e16fb70669b
Sudharsan M S <sudharzz3049@gmail.com>
2026-07-14 13:38:06 +0530

    Addition of insertion functions

---
+++ circularly_linkedlist.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+}*head;
+
+void display()
+{
+    struct Node *temp = head ->next;
+    if (temp == NULL)
+    {
+        printf("List is empty \n");
+        return;
+    }
+    do
+    {
+        printf("%d ->", temp->data);
+        temp = temp->next;
+    } while (temp != head->next);
+    
+printf("(back to %d)\n",temp->data);
+}
+
+void insfirst(int val)
+{
+    struct Node *newnode = malloc(sizeof(struct Node));
+    newnode -> data = val;
+    if (head -> next == NULL)
+    {
+        head -> next = newnode;
+        newnode -> next = newnode;
+    }
+    else{
+        struct Node *temp = head -> next;
+        while(temp -> next != head -> next)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newnode;
+        newnode -> next = head -> next;
+        head->next = newnode;
+    }
+    display();
+    printf("\n");
+}
+
+void inslast(int val)
+{
+    struct Node *newnode = malloc(sizeof(struct Node));
+    newnode -> data = val;
+    if (head -> next == NULL)
+    {
+        head -> next = newnode;
+        newnode -> next = newnode;
+    }
+    else{
+        struct Node *temp = head -> next;
+        while (temp -> next != head -> next){
+            temp = temp -> next;
+        }
+        temp -> next = newnode;
+        newnode -> next = head -> next;
+    }
+    display();
+    printf("\n");
+}
+
+void insspecific(int s, int val)
+{
+    struct Node *newnode = malloc(sizeof(struct Node));
+    newnode -> data = val;
+    struct Node *temp = head -> next;
+    while(temp-> next != head -> next && temp-> data != s)
+    {
+        temp = temp -> next;
+    }
+    if (temp -> data == s)
+    {
+        newnode -> next = temp -> next;
+        temp ->  next = newnode;
+        display();
+        printf("\n");
+    }
+    else {
+        printf("Element not found!");
+        printf("\n");
+    }
+}
+
+int main()
+{
+    head = malloc(sizeof(struct Node));
+    struct Node *first = malloc(sizeof(struct Node));
+    first -> data = 10;
+    head -> next = first;
+    struct Node *second = malloc(sizeof(struct Node));
+    second -> data = 20;
+    first -> next = second;
+    struct Node *third = malloc(sizeof(struct Node));
+    third -> data = 30;
+    second -> next = third;
+    third -> next = first;
+    insfirst(5);
+    inslast(100);
+    insspecific(30,50);
+}
